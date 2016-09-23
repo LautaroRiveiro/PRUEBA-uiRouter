2768f6019237aa73dc37009b890ecd0a6c384ebf
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-09-23 20:46:05 -0300

    Primer paso Creo un index simple con conexión angular. Como prueba un input con ng-model.

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module("miApp", ['ui.router']);
+
+/*app.config(function($stateProvider, $urlRouterProvider){
+	
+	$stateProvider
+	        .state('settings', {
+	            url: '/settings',
+	            templateUrl: 'templates/settings.html'
+	        })
+	        .state('settings.profile', {
+	            url: '/profile',
+	            templateUrl: 'templates/profile.html',
+	            controller: 'ProfileController'
+	        })
+	        .state('settings.account', {
+	            url: '/account',
+	            templateUrl: 'templates/account.html',
+	            controller: 'AccountController'
+	        });
+	
+	$urlRouterProvider.otherwise('/settings/profile');
+});*/
